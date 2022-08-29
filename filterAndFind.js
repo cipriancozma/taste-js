1bf60c78e7cc5cf1a5346fd464cbc2b939cfad34
Ciprian <cozma_ciprian19@yahoo.com>
2022-08-29 17:37:01 +0300

    added filter and find file

--- filterAndFind.js
+++ filterAndFind.js
@@ -4,3 +4,15 @@ const people = [
   { name: "susy", age: 30, position: "the boss" },
   { name: "anna", age: 35, position: "intern" },
 ];
+
+// filter -> returns a new array based on condition
+// find -> returns single instance, returns first match, if no match - undefined
+
+const young = people.filter((person) => person.age <= 25);
+console.log(young);
+
+const developer = people.find((person) => person.position == "developer");
+console.log(developer);
+
+const randomPerson = people.find((person) => person.age < 35);
+console.log(randomPerson);
