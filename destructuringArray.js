0569e361bbf13f04d21bab689b19bd9545507906
Ciprian <cozma_ciprian19@yahoo.com>
2022-09-01 10:32:35 +0300

    added destructuring arrays and objects

---
+++ destructuringArray.js
@@ -0,0 +1,11 @@
+const fruits = ["orange", "banana", "lemon"];
+const friends = ["john", "peter", "bob", "anna", "kelly"];
+
+const fruits1 = fruits[0];
+const fruits2 = fruits[1];
+const fruits3 = fruits[2];
+
+console.log(fruits1, fruits2, fruits3);
+
+const [john, peter, , anna] = friends;
+console.log(john, peter, anna);
