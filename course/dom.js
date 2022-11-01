6926e4150daca5c8ee0ffa5c9ff50742f6bbddc8
Ciprian <cozma_ciprian19@yahoo.com>
2022-11-01 17:21:03 +0200

    playing with dom 1

---
+++ course/dom.js
@@ -0,0 +1,21 @@
+// 14. DOM
+// useful to represent the elements of an HTML page
+// we can use JS to manipulate the DOM
+// object oriented
+// accesed using the document property of the window object
+
+// select a single element
+// document.getElementById()
+// document.querySelector()
+
+// select multiple elements
+// document.getElementsByClassName()
+// document.getElementsByTagName()
+// document.querySelectorAll()
+
+// 15. DOM - going through the DOM
+// up -> parentElement,  closest
+// siblings -> nextElementSibling, previousElementSibling
+// down -> children
+
+// 16. Create dinamic HTML elements
