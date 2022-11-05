e634644562f501ea471fea3d1ab5b863591e71b4
Ciprian <cozma_ciprian19@yahoo.com>
2022-11-05 18:24:07 +0200

    prepare for unique val ex

---
+++ practice/uniqueValues.js
@@ -0,0 +1,10 @@
+// return true only if all values are unique, otherwise false
+// using Array, Object, string method lastIndexOf()
+// no Set Data Structure
+
+const unique = (str) => {
+  return str;
+};
+
+console.log(unique("abcde")); // true
+console.log(unique("abacdefb")); //false
