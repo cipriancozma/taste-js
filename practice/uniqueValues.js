a8fc68078d187872be93daf29f5df057947bd396
Ciprian <cozma_ciprian19@yahoo.com>
2022-11-15 14:05:19 +0200

    practicing with unique values in a string

--- practice/uniqueValues.js
+++ practice/uniqueValues.js
@@ -3,8 +3,30 @@
 // no Set Data Structure
 
 const unique = (str) => {
-  return str;
+  let values = [];
+
+  for (let letter of str) {
+    if (values.indexOf(letter) !== -1) {
+      return false;
+    }
+    values = [...values, letter];
+  }
+  console.log(values);
+  return true;
 };
 
 console.log(unique("abcde")); // true
 console.log(unique("abacdefb")); //false
+
+const uniqueWithIdx = (str) => {
+  for (let i = 0; i < str.length; i++) {
+    // console.log(str.lastIndexOf(str[i]));
+    if (str.lastIndexOf(str[i]) !== i) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(uniqueWithIdx("abcde")); // true
+console.log(uniqueWithIdx("abacdefb")); // false
