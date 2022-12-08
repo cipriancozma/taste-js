8379975b170858b931aa006e92156c629842d98e
Ciprian <cozma_ciprian19@yahoo.com>
2022-12-08 19:19:36 +0200

    finished previous exercise with second value and started another one

--- practice/secondValue.js
+++ practice/secondValue.js
@@ -1,7 +1,12 @@
 function secondValue(arr) {
   let tempArr = [...new Set(arr)].sort((a, b) => a - b);
-
-  return tempArr;
+  if (tempArr.length < 2) {
+    return `${tempArr}`;
+  } else if (tempArr.length === 2) {
+    return `${tempArr[0]},${tempArr[1]}`;
+  } else {
+    return `${tempArr[1]},${tempArr[tempArr.length - 2]}`;
+  }
 }
 
 console.log(secondValue([1])); // 1
